40a18f9b6763a19469cea33138cc1a5915a1f6ed
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-06-16 14:17:07 +0200

    make repo and add guessing game

---
+++ casino-royale/0.guessing-game/guess.js
@@ -0,0 +1,31 @@
+(function () {
+    console.log('Let the guessing game begin');
+
+    function getRandomInt(max){
+        return Math.floor( Math.random() * max) + 1;
+    }
+
+    const allPeople = 20;
+    const secretNumber = getRandomInt(allPeople);
+
+    document.getElementById("userGuessLabel").innerHTML = `Between 1 and ${allPeople}`;
+
+    document.getElementById("run").addEventListener('click', 
+    function(){
+        const userGuess = document.getElementById("userGuess").value;
+        console.log(userGuess);
+
+        if (userGuess == secretNumber) {
+            document.getElementById("result").innerHTML = `Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`;
+            
+    
+        }
+
+        else {
+            document.getElementById("result").innerHTML = `Bummer... You guessed ${userGuess} and the secret number was ${secretNumber}.`;
+    
+        }
+   
+    });
+
+})();
